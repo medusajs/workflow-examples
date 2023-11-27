151f7a2b361078eb7ed6d46e5e1c33c4b55ba203
fPolic <mainacc.polic@gmail.com>
2023-11-27 20:57:22 +0000

    refactor: remove unused

--- notify-category-customers/src/repositories/customer-product-category.ts
+++ notify-category-customers/src/repositories/customer-product-category.ts
@@ -1,5 +1,3 @@
-import { FindOperator, FindOptionsWhere, ILike, In } from "typeorm";
-import { promiseAll } from "@medusajs/utils";
 import { dataSource } from "@medusajs/medusa/dist/loaders/database";
 
 import { CustomerProductCategory } from "../models";
@@ -7,8 +5,5 @@ import { CustomerProductCategory } from "../models";
 export const CustomerProductCategoryRepository = dataSource.getRepository(
   CustomerProductCategory
 );
-//   .extend({
-//     async add () {},
-//   });
 
 export default CustomerProductCategoryRepository;
